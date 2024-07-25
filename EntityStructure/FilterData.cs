71eca315652d9a0b21fa3f83749db6f6c234d272
Wilber Jose Matus Gonzalez <wilberj1987@gmail.com>
2024-07-25 16:26:27 -0600

    profiles-ext/i-sadav#3036 mejora en el FilterData In(string? propName, params int?[] values)

--- EntityStructure/FilterData.cs
+++ EntityStructure/FilterData.cs
@@ -10,6 +10,10 @@ namespace CAPA_DATOS
 		{
 			return new FilterData { PropName = propName, FilterType = "in", Values = values.Select(v => v?.ToString()).ToList() };
 		}
+		public static FilterData In(string? propName, params int?[] values)
+		{
+			return new FilterData { PropName = propName, FilterType = "in", Values = values.Select(v => v.GetValueOrDefault().ToString()).ToList() };
+		}
 		public static FilterData NotIn(string? propName, params object?[] values)
 		{
 			return new FilterData { PropName = propName, FilterType = "not in", Values = values.Select(v => v?.ToString()).ToList() };
