35dfc88a97a723a9dd5e95c03ab826276cdf9ed3
Wilber Jose Matus Gonzalez <wilberj1987@gmail.com>
2024-08-03 19:20:54 -0600

    cambio de definicion de default conexion TODO TEMPORTAL

--- EntityStructure/EntityClass.cs
+++ EntityStructure/EntityClass.cs
@@ -328,7 +328,7 @@ public abstract class TransactionalClass
 	{
 		return MTConnection;
 	}
-	internal void SetConnection(WDataMapper? wDataMapper)
+	public void SetConnection(WDataMapper? wDataMapper)
 	{
 		MTConnection = wDataMapper;
 	}
