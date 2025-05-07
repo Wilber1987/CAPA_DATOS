adffbea4af77f762961f218d052b3368a44c4033
Wilber jose matus <wilberj@gmail.com>
2025-05-07 15:34:09 -0600

    profiles-ext/i-sadav#4273 mejoras en acuerdos

--- EntityStructure/QueryClass.cs
+++ EntityStructure/QueryClass.cs
@@ -5,5 +5,10 @@ namespace APPCORE;
 public abstract class QueryClass : TransactionalClass
 {
 	public abstract string GetQuery();
-	public abstract List<T> Get<T>();
+	public abstract List<T> Get<T>();	
+	public T? Find<T>() 
+	{
+        var result =  Get<T>();
+       return result.Count > 0  ? result[0] : default;       
+    }
 }
